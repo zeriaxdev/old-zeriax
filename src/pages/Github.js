4ef79e5502f755371b7684e55445a068298aedf9
theZeriax <hedgegorg@gmail.com>
2022-01-07 19:25:55 +0200

    repos

--- src/pages/Github.js
+++ src/pages/Github.js
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
 import { useEffect, useState } from "react";
+import Linkify from "linkify-react";
+import "animate.css";
 
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const url = `https://api.github.com/users/theZeriax`;
+let username = "theZeriax";
+const url = `https://api.github.com/users/${username}`;
 
 const FetchUser = () => {
   const [data, setData] = useState(null);
@@ -21,7 +24,7 @@ const FetchUser = () => {
         className="
       min-w-2xl max-w-3xl mx-auto py-12 px-4
       sm:px-6 lg:py-16 lg:px-8 rounded-2xl 
-      bg-gray-400/20 dark:bg-zinc-900 dark:text-slate-50
+      bg-gray-300/20 dark:bg-zinc-900 dark:text-slate-50
       shadow-lg shadow-zinc-300/50 dark:shadow-zinc-900/50
       "
       >
@@ -113,11 +116,7 @@ const FetchUser = () => {
               </dl>
             </div>
           </div>
-          <div
-            className="
-            lg:col-span-1
-            "
-          >
+          <div className="lg:col-span-1">
             <div className="p-2 sm:px-0">
               <dl>
                 <dt className="text-sm leading-5 font-medium text-gray-500 dark:text-gray-400">
@@ -155,89 +154,167 @@ const FetchUser = () => {
   );
 };
 
-// const FetchRepos = () => {
-//   const [data, setData] = useState(null);
+const FetchRepository = () => {
+  const [data, setData] = useState(null);
 
-//   useEffect(() => {
-//     fetch(`${url}/repos`)
-//       .then((res) => res.json())
-//       .then((data) => setData(data));
-//   }, []);
+  useEffect(() => {
+    fetch(`${url}/repos`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
 
-//   return data ? (
-//     <div className="max-w-screen-xl mx-auto rounded-3xl">
-//       <div className="flex flex-col">
-//         <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 rounded-3xl">
-//           <div
-//             className="
-//           align-middle inline-block min-w-full rounded-3xl
-//           sm:rounded-lg border-b border-gray-200 dark:border-gray-700
-//           "
-//           >
-//             <table className="min-w-full">
-//               <thead>
-//                 <tr>
-//                   <th
-//                     className="
-//                     px-6 py-4 bg-gray-50 text-left rounded-t-3xl
-//                     text-xs leading-4 font-medium text-gray-500 uppercase
-//                     tracking-wider dark:text-gray-400 dark:bg-zinc-900
-//                     "
-//                     colSpan="2"
-//                   >
-//                     Repositories
-//                   </th>
-//                 </tr>
-//               </thead>
-//               <tbody className="bg-white dark:bg-zinc-900">
-//                 {data.map((repo) => (
-//                   <tr className={`repoID:${repo.id}`}>
-//                     <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 dark:border-zinc-700">
-//                       <div className="flex items-center">
-//                         <div className="flex-shrink-0 h-10 w-10">
-//                           <img
-//                             className="h-10 w-10 rounded-full"
-//                             src={repo.owner.avatar_url}
-//                             alt={repo.owner.login}
-//                           />
-//                         </div>
-//                         <div className="ml-4">
-//                           <div className="text-sm leading-5 font-medium text-gray-900 dark:text-gray-100">
-//                             <a
-//                               href={repo.owner.html_url}
-//                               className="underline text-gray-600 dark:text-gray-400"
-//                             >
-//                               {repo.owner.login}
-//                             </a>
-//                           </div>
-//                           <div className="text-sm leading-5 text-gray-500 dark:text-gray-400">
-//                             {repo.name}
-//                           </div>
-//                         </div>
-//                         <div className="ml-7 text-gray-600 dark:text-zinc-400">
-//                           {repo.fork ? <BiGitRepoForked /> : ""}
-//                         </div>
-//                       </div>
-//                     </td>
-//                   </tr>
-//                 ))}
-//               </tbody>
-//             </table>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   ) : (
-//     <div>Loading...</div>
-//   );
-// };
+  return data
+    ? data.map((repo) => {
+        return (
+          <div className="mb-5">
+            <div className="bg-white dark:bg-zinc-800 dark:text-slate-50">
+              <div
+                className="
+    min-w-2xl max-w-3xl mx-auto py-12 px-4
+    sm:px-6 lg:py-16 lg:px-8 rounded-2xl 
+    bg-gray-300/20 dark:bg-zinc-900 dark:text-slate-50
+    shadow-lg shadow-zinc-300/50 dark:shadow-zinc-900/50
+    "
+              >
+                <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+                  <div className="lg:col-span-1">
+                    <div className="px-2 sm:px-0">
+                      <h3 className="text-2xl leading-8 font-extrabold text-gray-900 dark:text-gray-100">
+                        <a href={repo.html_url} className="url">
+                          {repo.name}
+                        </a>
+                      </h3>
+                      <Linkify tagName="p">
+                        <p className="mt-3 text-md leading-6 text-gray-500 dark:text-gray-400">
+                          {repo.description
+                            ? repo.description
+                            : "No description"}
+                        </p>
+                      </Linkify>
+
+                      <div className="mt-6 flex items-center">
+                        <div className="flex-shrink-0">
+                          <img
+                            className="h-12 w-12 rounded-full"
+                            src={repo.owner.avatar_url}
+                            alt={repo.owner.login}
+                          />
+                        </div>
+                        <div className="ml-3">
+                          <div className="text-base leading-6 font-medium text-gray-900 dark:text-gray-100">
+                            {repo.owner.login}
+                          </div>
+                          <div className="mt-1 text-sm leading-5 text-gray-500 dark:text-gray-400">
+                            <a href={repo.html_url} className="underline">
+                              {repo.full_name}
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="lg:col-span-0 md:col-span-1">
+                    <div className="p-2 sm:px-0">
+                      <dl>
+                        <dt className="text-sm leading-5 font-medium text-gray-500 dark:text-gray-400 pt-2.5">
+                          Language
+                        </dt>
+                        <dd className="mt-1 text-sm leading-5 text-gray-900 dark:text-gray-100">
+                          {repo.language ? (
+                            <span
+                              className={`inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-gray-200 dark:bg-zinc-800 dark:text-gray-100`}
+                            >
+                              <img
+                                src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
+                                  repo.language
+                                    ? repo.language.toLowerCase()
+                                    : "github"
+                                }/${
+                                  repo.language
+                                    ? repo.language.toLowerCase()
+                                    : "github"
+                                }-original.svg`}
+                                alt=""
+                                className="h-4 w-4 mr-1"
+                              />
+                              {repo.language}
+                            </span>
+                          ) : (
+                            "No language"
+                          )}
+                        </dd>
+                        <dt className="text-sm leading-5 font-medium text-gray-500 dark:text-gray-400 pt-2.5">
+                          Created at
+                        </dt>
+                        <dd className="mt-1 text-sm leading-5 text-gray-900 dark:text-gray-100">
+                          {new Date(repo.created_at).toLocaleDateString(
+                            "en-UK",
+                            {
+                              year: "numeric",
+                              month: "long",
+                              day: "numeric",
+                              hour: "numeric",
+                              minute: "numeric",
+                            }
+                          )}
+                        </dd>
+                        <dt className="text-sm leading-5 font-medium text-gray-500 dark:text-gray-400 pt-2.5">
+                          Updated at
+                        </dt>
+                        <dd className="mt-1 text-sm leading-5 text-gray-900 dark:text-gray-100">
+                          {new Date(repo.updated_at).toLocaleDateString(
+                            "en-UK",
+                            {
+                              year: "numeric",
+                              month: "long",
+                              day: "numeric",
+                              hour: "numeric",
+                              minute: "numeric",
+                            }
+                          )}
+                        </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="lg:col-span-1">
+                    <div className="p-2 sm:px-0">
+                      <dl>
+                        <dt className="text-sm leading-5 font-medium text-gray-500 dark:text-gray-400 pt-2.5">
+                          Open Issues
+                        </dt>
+                        <dd className="mt-1 text-sm leading-5 text-gray-900 dark:text-gray-100">
+                          {repo.open_issues}
+                        </dd>
+                        <dt className="text-sm leading-5 font-medium text-gray-500 dark:text-gray-400 pt-2.5">
+                          Watchers
+                        </dt>
+                        <dd className="mt-1 text-sm leading-5 text-gray-900 dark:text-gray-100">
+                          {repo.watchers}
+                        </dd>
+                        <dt className="text-sm leading-5 font-medium text-gray-500 dark:text-gray-400 pt-2.5">
+                          Forks
+                        </dt>
+                        <dd className="mt-1 text-sm leading-5 text-gray-900 dark:text-gray-100">
+                          {repo.forks}
+                        </dd>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })
+    : "Loading...";
+};
 
 const Github = () => {
   return (
     <>
       <Header />
       <div className="flex flex-col justify-center items-center">
-        <div className="Github bg-white dark:bg-zinc-800 dark:text-slate-50">
+        <div className="github bg-white dark:bg-zinc-800 dark:text-slate-50">
           <div className="container mx-auto px-4">
             <h1 className="text-center mt-10">GitHub Stats</h1>
             <div className="flex justify-between items-center py-3">
@@ -247,14 +324,20 @@ const Github = () => {
                 </div>
               </div>
             </div>
-            {/* <h2 className="text-center">Repositories</h2>
+            <h2 className="text-center">Repositories</h2>
             <div className="flex justify-between items-center py-3">
               <div className="flex items-center flex-col md:flex-row">
-                <div className="items-center">
-                  <FetchRepos />
+                <div className="m-5">
+                  <FetchRepository>
+                    {({ data, loading, error }) => {
+                      if (loading) return "Loading...";
+                      if (error) return `Error! ${error.message}`;
+                      return <FetchRepository data={data} />;
+                    }}
+                  </FetchRepository>
                 </div>
               </div>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
